a58949f2c0f56ce814d609768811318b03801e20
takahiro sakai <takahirosakai@takahironoMacBook-Air.local>
2024-12-17 08:35:49 +0900

    update

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -1,6 +1,6 @@
 <meta name="robots" content="noindex" />
 
-@if ( false )
+@if ( true )
 
 
     @php
@@ -10,12 +10,12 @@
 
         # メタのデフォルト画像
         $meta_default_image = $meta_gachas->count() > 0
-        ? $meta_gachas[0]->image_path : asset('storage/site/image/logo512.png');
+        ? $meta_gachas[0]->image_path : asset('storage/site/image/logo.png');
 
         $meta_title = isset( $meta_title ) ? $meta_title.'-'.config('app.name') :
         config('app.name')."|オンラインオリパ・ネットオリパを24時間365日楽しめる！国内送料は無料！ ";
         $meta_description = isset( $meta_description ) ? $meta_description :
-        "オンラインオリパ引くなら".config('app.name')."! 高確率、爆アドガチャを多数ご用意しています。ポケカ・ワンピースなど人気オリパを24時間365日楽しめます。国内送料無料で、低コストガチャからハイリスクハイリターンなガチャなど楽しみ方は自由自在！ ";
+        "オンラインオリパ引くなら".config('app.name')."! 高確率、爆アドガチャを多数ご用意。人気オリパを24時間365日楽しめます。国内送料無料！ ";
         $meta_image = isset( $meta_image ) ? $meta_image :
         $meta_default_image;//メタのデフォルト画像
 
@@ -24,7 +24,7 @@
 
 
     <meta  name="description" content="{{ $meta_description }}">
-    <meta  name="keywords" content="オンラインオリパ,ネットオリパ,オリパ,ポケカ,ワンピース,ガンダムアーセナルベース,カード,ガチャ,トレカ, ">
+    <meta  name="keywords" content="オンラインオリパ,ネットオリパ,オリパ,カード,ガチャ,トレカ, ">
 
     <meta property="og:title" content="{{ $meta_title }}" />
     <meta property="og:description" content="{{ $meta_description }}" />
