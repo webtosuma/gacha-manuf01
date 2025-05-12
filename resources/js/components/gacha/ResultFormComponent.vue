67d6495c9a42ccf11970aeaf6316f459dd250890
takahiro sakai <takahirosakai@takahironoMacBook-Air.local>
2025-05-13 03:15:41 +0900

    更新

--- resources/js/components/gacha/ResultFormComponent.vue
+++ resources/js/components/gacha/ResultFormComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-dark">
 
         <loading-cover-component :loading="loading" />
 
--- resources/js/components/user_prize/FormComponent.vue
+++ resources/js/components/user_prize/FormComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="">
+    <div class="text-dark">
 
         <!--ボトムメニュー-->
         <div v-if="bottom_menu == 'true'"
