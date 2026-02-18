7e16220613f529806c97197fd01b9c3ffdc3fca0
takahiro sakai <takahirosakai@takahironoMacBook-Air.local>
2026-02-18 16:16:39 +0900

    オリスタ/ガチャマックス設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,20 +22,20 @@ class DatabaseSeeder extends Seeder
 
 
         /* テスト用 */
-        $this->call(UserSeeder::class);//テスト用ユーザー
-        $this->call(GachaCategorySeeder::class);//ガチャのカテゴリーグループ
-        $this->call(MovieSeeder::class);//演出動画
-        $this->call(PointSailSubscriptionSeeder::class);//販売用サブスクプラン
-        $this->call(GachaSeeder::class);//ガチャ
-        $this->call(GachaDiscriptionSeeder::class);//ガチャ詳細情報
-        $this->call(PrizeSeeder::class);//景品
-        $this->call(GachaPrizeSeeder::class);//各ガチャの景品
-        $this->call(GachaRankMovieSeeder::class);//各ガチャの演出動画設定
-        $this->call(InfomationSeeder::class);//お知らせ
-        $this->call(TicketSailSeeder::class);//販売用チケット
-        $this->call(StoreSeeder::class);//販売商品
-        $this->call(CouponSeeder::class);//クーポン
-        $this->call(StoreItemSeeder::class);//EC販売アイテム
+        // $this->call(UserSeeder::class);//テスト用ユーザー
+        // $this->call(GachaCategorySeeder::class);//ガチャのカテゴリーグループ
+        // $this->call(MovieSeeder::class);//演出動画
+        // $this->call(PointSailSubscriptionSeeder::class);//販売用サブスクプラン
+        // $this->call(GachaSeeder::class);//ガチャ
+        // $this->call(GachaDiscriptionSeeder::class);//ガチャ詳細情報
+        // $this->call(PrizeSeeder::class);//景品
+        // $this->call(GachaPrizeSeeder::class);//各ガチャの景品
+        // $this->call(GachaRankMovieSeeder::class);//各ガチャの演出動画設定
+        // $this->call(InfomationSeeder::class);//お知らせ
+        // $this->call(TicketSailSeeder::class);//販売用チケット
+        // $this->call(StoreSeeder::class);//販売商品
+        // $this->call(CouponSeeder::class);//クーポン
+        // $this->call(StoreItemSeeder::class);//EC販売アイテム
 
 
 
